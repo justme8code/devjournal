f1176bffdbfeb69f3906061476130289f012f468
justme8code <jvmMonster1@gmail.com>
2025-02-13 12:39:10 +0100

    Just An Update

--- app/api_urls.ts
+++ app/api_urls.ts
@@ -1,4 +1,4 @@
-const TECH_TIDE_BASE_URL : string = "https://techtideapi10.wittysea-6a4048d5.westus2.azurecontainerapps.io/api";
+const TECH_TIDE_BASE_URL : string = "https://techtideapicontainer.livelytree-1634fab4.westus2.azurecontainerapps.io/api";
 const TECH_TIDE_BLOGS_URL : string  = `${TECH_TIDE_BASE_URL}/blogs`
 const TECH_TIDE_USER_BLOG : string = `${TECH_TIDE_BASE_URL}/users/blogs`
 const TECH_TIDE_AUTH_URL : string = `${TECH_TIDE_BASE_URL}/auth`
