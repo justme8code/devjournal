e6f05e37ceabee2b05de74292869bf829ad9206f
justme8code <jvmMonster1@gmail.com>
2025-02-13 11:02:14 +0100

    Updated next.config.ts to allow any host name and allow any path names for loading images or any other content

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: "standalone"
+    output: "standalone",
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*',  // Allow any hostname
+                port: '',
+                pathname: '/**',  // Allow any pathname
+                search: '',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
