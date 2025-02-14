75faa6696422293b134d688e88eae6cea473e7b1
justme8code <jvmMonster1@gmail.com>
2025-02-14 18:02:17 +0100

    Update

--- middleware.ts
+++ middleware.ts
@@ -2,28 +2,22 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 const COOKIE_NAME = 'tech-tide-auth-cookie';
+
 export function middleware(request: NextRequest) {
-    console.log('Running middleware');
-    console.log('Cookies:', request.cookies);
-
     const jwt = request.cookies.get(COOKIE_NAME)?.value;  // Access the cookie
-
     const path = request.nextUrl.pathname;  // Current page path
 
-    // If no JWT is found and user is not on the login page, redirect to login
-    if (!jwt && path === '/tech-tider/create-new-content') {
+    // Only secure the /tech-tider/create-new-content route
+    if (path === '/tech-tider/create-new-content' && !jwt) {
+        // If no JWT token and trying to access create-new-content, redirect to login
         return NextResponse.redirect(new URL('/tech-tider', request.url));
     }
 
-    if(jwt && path === '/tech-tider') {
-        return NextResponse.redirect(new URL('/tech-tider/create-new-content', request.url));
-    }
-
-    // Allow the request to continue if no redirects are needed
+    // Allow the request to continue if the user is authorized or if it's any other route
     return NextResponse.next();
 }
 
-// Adjusted config to apply middleware to both the root and project routes
+// Adjusted config to apply middleware only to the create-new-content route
 export const config = {
-    matcher: ['/tech-tider', '/tech-tider/create-new-content'], // The correct matcher pattern
+    matcher: '/tech-tider/create-new-content',  // Only apply to this route
 };
