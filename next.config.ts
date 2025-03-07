9b8824752503c108aa39e52c0a760fc16d25ebb5
justme8code <bjmay302004@gmail.com>
2025-03-07 14:12:00 +0100

    Update

--- next.config.ts
+++ next.config.ts
@@ -7,31 +7,7 @@ const nextConfig: NextConfig = {
             {
                 protocol: 'https',
                 hostname: '*',
-                port: '',
-                pathname: '/**',
-                search: '',
-            },
-            {
-                protocol:'https',
-                hostname:'https://lh3.googleusercontent.com/fife/**'
-            },
-            {
-                protocol:'https',
-                hostname: 'th.bing.com',
-            },
-            {
-                protocol:'https',
-                hostname:'tse4.mm.bing.net'
-            },
 
-            {
-                protocol:"https",
-                hostname:"static.vecteezy.com/**",
-            },
-
-            {
-                protocol:"https",
-                hostname:"encrypted-tbn0.gstatic.com/**"
             }
         ],
     },
