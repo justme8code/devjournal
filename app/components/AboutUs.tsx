1330a634a27bfa96b4522bb2cb22675f51e4478c
justme8code <jvmMonster1@gmail.com>
2025-03-11 18:01:13 +0100

    Little Bug Fix & Update

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -17,7 +17,7 @@ export const AboutUs = () => {
                 showInfo && (
                     <AnimatedPopUp onClickClose={() => setShowInfo(false)}>
                         <div className="text-gray-700 space-y-4">
-                            <h2 className="text-2xl font-bold text-gray-800">About TechTide</h2>
+                            <h2 className="text-2xl font-bold text-gray-800">About DevJournal</h2>
                             <p>
                                 DevJournal is a platform dedicated to help developers and tech enthusiasts
                                 learn.
